ac88db19a5625ec33c453db65f9550ce256c47f9
Tom Sharkey <thomas_sharkey@comcast.com>
2018-04-26 16:09:20 -0400

    foobar

--- test/unit/hello_test.js
+++ test/unit/hello_test.js
@@ -7,7 +7,7 @@ const Hello = require('../../lib/hello.js');
 lab.experiment('test greetings', () => {
 
     lab.test('greets with name ', (done) => {
-
+        console.log("hello")
         Code.expect(Hello('Homer')).to.equal('Hello, Homer!');
         done();
     });
