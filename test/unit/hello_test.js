8ec7ad2622041f8c6dac081ca81d225c61f5de14
Tom Sharkey <tsharkey@users.noreply.github.com>
2018-04-27 14:40:13 -0400

    Update hello_test.js

--- test/unit/hello_test.js
+++ test/unit/hello_test.js
@@ -7,7 +7,7 @@ const Hello = require('../../lib/hello.js');
 lab.experiment('test greetings', () => {
 
     lab.test('greets with name ', (done) => {
-        console.log("hello")
+        console.log("hello world")
         Code.expect(Hello('Homer')).to.equal('Hello, Homer!');
         done();
     });
